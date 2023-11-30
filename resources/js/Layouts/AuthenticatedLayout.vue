cf41c89f059b6d7a2919afeb417eecd68baf4168
Poofs <77813904+PoofyPloop@users.noreply.github.com>
2023-11-30 14:46:04 -0500

    Nav modification

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -160,6 +160,7 @@ watch(() => searchOptions.value, async (newSearch, oldSearch) => {
 
                     <!-- Responsive Settings Options -->
                     <div class="pt-4 pb-1 border-t border-gray-200">
+
                         <div class="px-4">
                             <div class="font-medium text-base text-gray-800">
                                 {{ $page.props.auth.user.name }}
