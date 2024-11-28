c66ce5fd6ec8ce94247a1c262d08a6238a981189
Rawad <rawad.haddad@mohawkcollege.ca>
2024-11-28 02:06:39 -0500

    removing pusher

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,15 +25,3 @@ createInertiaApp({
 
 });
 console.log("app.js log here")
-
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
- 
-window.Pusher = Pusher;
- 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
-});
